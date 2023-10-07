d213df17bea7a35e68f6e39e2c46486836d57df3
Ankit Verma <a.verma0827@gmail.com>
2023-10-07 16:34:29 +0530

    showcard changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,24 @@
 import {Link} from 'react-router-dom'
 import '../css/header.css'
 
-const Header=()=>{
+ const Header=()=>{
+
+    
+
     return(
         <div className="header-div">
             <div className="logo-div">
                 <h3>Comapany Name</h3>
             </div>
             <div className="link-div">
-            <ul className='ul-div'>
-                <li><Link to="/" className='Link'>Home</Link></li>
-                <li><Link to="/About" className='Link'>About us</Link></li>
-                <li><Link to="/Contact" className='Link'>Contact us</Link></li>
-            </ul>
-            <button className='btn' >Login</button>
+
+               
+                      <ul className='ul-div'>
+                      <li><Link to="/" className='Link'>Home</Link></li>
+                      <li><Link to="/About" className='Link'>About us</Link></li>
+                      <li><Link to="/Contact" className='Link'>Contact us</Link></li>
+                  </ul>
+           
             </div>
     
         </div>
